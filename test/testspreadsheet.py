1174596b46d285449a1414437371f1cc9cd7b9c4
Juippii <A1801097@edu.lapinamk.fi>
2024-10-29 18:44:48 +0200

    US5 - test3

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -65,3 +65,9 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set('A1', '=1+3.5')
         self.assertEqual('#Error', spreadsheet.evaluate('A1'))
+
+    #If the cell "A1" contains "=1/0", the result of its evaluation is "#Error".
+    def test_evaluate_division_by_zero(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set('A1', '=1/0')
+        self.assertEqual('#Error', spreadsheet.evaluate('A1'))
