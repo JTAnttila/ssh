6fcae8f1a94cae4bcf8e0c59adafca1914ee9043
Juippii <A1801097@edu.lapinamk.fi>
2024-10-29 17:27:10 +0200

    US1 - test2

--- spreadsheet.py
+++ spreadsheet.py
@@ -15,5 +15,9 @@ class SpreadSheet:
         value = self.get(cell)
         if value.isdigit():
             return int(value)
-        return value
+        try:
+            float(value)
+            return '#Error'
+        except ValueError:
+            return value
 
